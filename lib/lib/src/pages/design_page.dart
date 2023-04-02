a6eacaa0ef18b34461b0c433a573cca6ea97208c
DaniloBustamanx <127712414+DaniloBustamanx@users.noreply.github.com>
2023-04-01 21:09:00 -0600

    Add files via upload

---
+++ lib/lib/src/pages/design_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+class DesignPage extends StatelessWidget {
+  const DesignPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Danilo'),
+      ),
+    );
+  }
+}
